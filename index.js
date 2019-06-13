ff44ca4070c72b2d6ddeb64949775bd0d9db3034
Venkatesan Duraikannu <venkat.adal.us@gmail.com>
2019-06-13 04:53:35 -0700

    upd

--- index.js
+++ index.js
@@ -11,25 +11,14 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .post('/callback', (req, res) => {
-    const body = req.body || {};
+  .get('/callback', (req, res) => {
+    const { query: body } = req;
     if (body.code && body.id_token) {
       const idTokenData = jwtDecode.decode(body.id_token);
       console.log(idTokenData);
       res.render('pages/callback', {
         code: body.code,
         idtokenInfo: JSON.stringify(idTokenData, null, 4),
-        idt: {
-          "kid": "AIDOPK1",
-          "alg": "RS256",
-
-          "iss": "https://appleid.apple.com",
-          "aud": "jp.yauth.signin.service3",
-          "exp": 1560420369,
-          "iat": 1560419769,
-          "sub": "000321.9a806ce26a0546e1be8ff23525193a34.0220",
-          "at_hash": "RIYyLUoU5AXgtcDEsnXGVA"          
-        },
         state: body.state});
     } else {
       res.render('pages/index');
