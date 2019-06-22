18b46e1496c6acaf11d959ea57677a19f3508375
Walmik Deshpande <walmik@users.noreply.github.com>
2019-06-21 20:36:25 -0700

    Add files via upload

---
+++ public/widget.js
@@ -0,0 +1,26 @@
+(function() {
+  'use strict';
+
+  window.PYPL = window.PYPL || {};
+  window.PYPL.initPayouts = function(config) {
+    var iFrame = document.createElement('iframe');
+    var src = 'https://localhost.paypal.com/signin/payments';
+    var defaultProps = {
+      height: 700,
+      style: 'width: 325px;',
+      name: 'injectedPayouts',
+      frameborder: 0,
+      scrolling: 'no',
+      sandbox: 'allow-same-origin allow-scripts allow-popups allow-forms allow-top-navigation'
+    };
+    var queryParams = '?';
+    for (var param in config) {
+      queryParams = queryParams + param + '=' + encodeURIComponent(config[param]);
+    }
+    defaultProps.src = src + queryParams;
+    Object.keys(defaultProps).forEach(function(attr) {
+      iFrame.setAttribute(attr, defaultProps[attr]);
+    });
+    document.getElementById(config.parentId).appendChild(iFrame);
+  };
+}());
