87af9d8ecf8c751bb1c21321cebd876aa9cb410e
Karthick Gunasekaran <kagunasekaran@paypal.com>
2019-07-26 01:18:57 -0700

    return to a function

--- public/js/widget.js
+++ public/js/widget.js
@@ -2,7 +2,6 @@
     'use strict';
     window.PayPal = window.PayPal || {};
     window.PayPal.initCustomPayPalApp = function(config) {
-
     var iFrame = document.createElement('iframe');
     var src = config.src;
     var defaultProps = {
@@ -35,19 +34,19 @@
                     // code block
                     //console.log('data',data);
                     //return config.onLoad(data);
-                    PayPal.Onload(data);
+                    onWidgetLoad(data);
                     break;
                 case 'ERROR':
                     // code block
-                    PayPal.OnServerError(data);
+                    onServerError(data);
                     break;
                 case 'EXIT':
                     // code block
-                    PayPal.OnUserExit(data);
+                    onUserExit(data);
                     break;
                 case 'COMPLETE':
                     // code block
-                    PayPal.OnUserComplete(data);
+                    onUserComplete(data);
                     break;
                 default:
                 // code block
@@ -56,156 +55,29 @@
 
         },false);
 
-        // setInterval(function() {
-        //     if (window.addEventListener) {
-        //         window.addEventListener("message", listenMessage, false);
-        //     } else {
-        //         window.attachEvent("onmessage", listenMessage);
-        //     }
-        // },1000);
 
-        // const onPostMessageHandler = function(event) {
-        //     let postMessage = (event && event.data) || {};
-        //     if (!postMessage && !postMessage.data) {
-        //         console.log('noMessage');
-        //         return;
-        //     }
-        //     console.log('postMessage',postMessage);
-        //     if(typeof config.onLoad === 'function'){
-        //
-        //
-        //
-        //         // var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
-        //         // var eventer = window[eventMethod];
-        //         // var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "LOADED";
-        //         //
-        //         // eventer(messageEvent,function(e) {
-        //         //     console.log('parent received message!:  ',e.data);
-        //         // },false);
-        //         // config.onLoad();
-        //     }
-        //     // if(typeof config.onServerError === 'function'){
-        //     //     return this.hasServerError = true;
-        //     // }
-        //     // if(typeof config.onUserExit === 'function') {
-        //     //     return this.hasUserExit = true;
-        //     // }
-        //     // if(typeof config.onComplete === 'function'){
-        //     //     return this.hasUserComplete = true;
-        //     // }
-        // };
-        // window.addEventListener('message', onPostMessageHandler);
-
-        PayPal.Onload = function(data){
+        const onWidgetLoad = function(data){
             if(typeof config.onLoad === 'function'){
                 return config.onLoad(data)
             }
 
         };
-        PayPal.OnServerError = function(data){
+       const onServerError = function(data){
             if(typeof config.onError === 'function'){
                 return config.onError(data)
             }
         };
-        PayPal.OnUserExit = function(data){
+        const onUserExit = function(data){
             if(typeof config.onExit === 'function'){
                 return config.onExit(data)
             }
         };
-        PayPal.OnUserComplete = function(data){
-            if(typeof config.onExit === 'function'){
-                return config.onExit(data)
+       const onUserComplete = function(data){
+            if(typeof config.onComplete === 'function'){
+                return config.onComplete(data)
             }
         };
-        // setInterval(function() {
-        //     // Send the message "Hello" to the parent window
-        //     // ...if the domain is still "davidwalsh.name"
-        //     parent.postMessage(onPostMessageHandler, window.location);
-        // },1000);
-
-
-
-
-
-
-
     };
 
-
-    // function listenMessage(msg) {
-    //     console.log('calling after callback',msg);
-    //     return msg.data;
-    // }
-
-    //return config.onLoad(listenMessage);
 })();
 
-
-
-
-
-// (function() {
-//   'use strict';
-//   const getRandomCharacters = len => Math.random().toString(36).slice(-len);
-//
-//   window.PYPL = window.PYPL || {};
-//   window.PYPL.initPayouts = function(config) {
-//     var iFrame = document.createElement('iframe');
-//     var object = document.createElement('object');
-//     var src = config.src || 'https://login.paypal.com/signin/payments';
-//     var defaultProps = {
-//       height: config.height || 700,
-//       style: 'width: 355px;',
-//       name: 'injectedPayouts',
-//       frameborder: 0,
-//       scrolling: 'no',
-//       sandbox: 'allow-same-origin allow-scripts allow-popups allow-forms allow-top-navigation'
-//     };
-//     var queryParams = '?';
-//     for (var param in config) {
-//       queryParams = queryParams + param + '=' + encodeURIComponent(config[param]) + '&';
-//     }
-//     defaultProps.src = src + queryParams;
-//     Object.keys(defaultProps).forEach(function(attr) {
-//       iFrame.setAttribute(attr, defaultProps[attr]);
-//     });
-//     document.getElementById(config.parentId).appendChild(iFrame);
-//   };
-//
-//   window.PYPL.getSignUpDetails = function(config){
-//
-//
-//
-//   };
-//
-//   window.PYPL.getPaymentDetails = function(config){
-//
-//
-//
-//   };
-//
-//
-//   window.PYPL.setPaymentMethods = function(config){
-//
-//
-//
-//   };
-//   window.PYPL.setPaymentMethods = function(config){
-//
-//
-//
-//   };
-//
-//
-//   window.PYPL.getExternalId = function() {
-//     return [
-//       getRandomCharacters(8),
-//       getRandomCharacters(4),
-//       getRandomCharacters(4),
-//       getRandomCharacters(4),
-//       getRandomCharacters(12),
-//     ]
-//       .join('-')
-//       .toUpperCase();
-//   }
-// }());
