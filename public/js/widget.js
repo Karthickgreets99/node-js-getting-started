074a1ca7d7c26969f4fc1ffdcee5085436449118
Karthick Gunasekaran <kagunasekaran@paypal.com>
2019-07-31 12:16:15 -0700

    return to a function

--- public/js/widget.js
+++ public/js/widget.js
@@ -17,7 +17,7 @@
         var queryParams = '?';
         for (var param in config) {
             queryParams =
-                queryParams + param + domain + '=' + encodeURIComponent(config[param]) + '&';
+                domain + queryParams + param + '=' + encodeURIComponent(config[param]) + '&';
         }
         console.log('queryParams', queryParams);
         console.log('defaultProps', defaultProps);
