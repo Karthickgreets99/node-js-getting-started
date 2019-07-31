b793bbbe83e95d2e79c631e6d73e290a24663522
Karthick Gunasekaran <kagunasekaran@paypal.com>
2019-07-30 17:09:56 -0700

    return to a function

--- public/js/widget.js
+++ public/js/widget.js
@@ -1,42 +1,40 @@
-(function() {
+(function () {
     'use strict';
     window.PayPal = window.PayPal || {};
-    window.PayPal.initCustomPayPalApp = function(config) {
-    var iFrame = document.createElement('iframe');
-    var src = config.src;
-    var defaultProps = {
-        height: config.height || 700,
-        style: 'width: 355px;',
-        name: 'injectedCustomPayPalApp',
-        frameborder: 0,
-        scrolling: 'no',
-        sandbox:
-            'allow-same-origin allow-scripts allow-popups allow-forms allow-top-navigation',
-    };
-    var queryParams = '?';
-    for (var param in config) {
-        queryParams =
-            queryParams + param + '=' + encodeURIComponent(config[param]) + '&';
-    }
-    defaultProps.src = src + queryParams;
-    Object.keys(defaultProps).forEach(function(attr) {
-        iFrame.setAttribute(attr, defaultProps[attr]);
-    });
+    window.PayPal.initCustomPayPalApp = function (config) {
+        var iFrame = document.createElement('iframe');
+        var src = config.src;
+        var defaultProps = {
+            height: config.height || 700,
+            style: 'width: 355px;',
+            name: 'injectedCustomPayPalApp',
+            frameborder: 0,
+            scrolling: 'no',
+            sandbox:
+                'allow-same-origin allow-scripts allow-popups allow-forms allow-top-navigation',
+        };
+        var queryParams = '?';
+        for (var param in config) {
+            queryParams =
+                queryParams + param + '=' + encodeURIComponent(config[param]) + '&';
+        }
+        defaultProps.src = src + queryParams;
+        Object.keys(defaultProps).forEach(function (attr) {
+            iFrame.setAttribute(attr, defaultProps[attr]);
+        });
 
-    var postMessageKey = ['LOADED','ERROR','EXIT', 'COMPLETE'];
-    document.getElementById(config.parentId).appendChild(iFrame);
+        document.getElementById(config.parentId).appendChild(iFrame);
 
-        window.addEventListener('message',function(e) {
+        window.addEventListener('message', function (e) {
+            var postMessageKey = ['LOADED', 'ERROR', 'EXIT', 'COMPLETE'];
             var key = e.message ? 'message' : 'data';
-            var data =  e[key];
-            if(data){
-                var checkValue =  data.split('_');
-                //console.log('array',checkValue[0],checkValue[1]);
+            var data = e[key];
+            if (data) {
+                var checkValue = data.split('_',1);
+                console.log('checkvalue',checkValue);
                 var value = postMessageKey.indexOf(checkValue[1]) > -1 ? data : '';
-                //console.log('value:',value);
             }
-            //console.log('value:',value);
-            switch(checkValue[1]) {
+            switch (checkValue[1]) {
                 case 'LOADED':
                     config.onLoad && config.onLoad(value);
                     break;
@@ -57,7 +55,7 @@
             }
 
 
-        },false);
+        }, false);
 
 
     };
